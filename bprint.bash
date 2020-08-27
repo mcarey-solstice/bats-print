8e7113560fd7105ee234e775a846b1dd893f2656
Mike Carey <32496966+mike-carey@users.noreply.github.com>
2020-08-27 11:51:52 -0500

    Updates script to be shellcheck compliant

--- bprint.bash
+++ bprint.bash
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 function copy_function() {
   test -n "$(declare -f "$1")" || return
   eval "${_/$1/$2}"
@@ -12,8 +14,11 @@ rename_function run _run
 unset copy_function
 unset rename_function
 
+# shellcheck disable=SC2154
+# https://github.com/koalaman/shellcheck/wiki/SC2154
+# Variables supplied by BATS
 function run() {
-  _run $*
+  _run "$@"
   echo "status: $status"
   echo "output: '$output'"
 }
